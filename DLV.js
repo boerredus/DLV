61202e3ff1d8f07d3c73044cd65c570cb75bcbcd
João <joao.guimaraes.alves.vaz@gmail.com>
2021-04-02 16:01:19 -0300

    Complete redesign

--- DLV.js
+++ DLV.js
@@ -1,4 +1,26 @@
-function DLV(videoElement, newSrc, seconds, attributes = [], onEnded = () => {}) {
+const _defaultSwap = (newVideo, oldVideo) => {
+    if (!(newVideo in document.body))
+        document.body.append(newVideo)
+
+    oldVideo.controls = false;
+    oldVideo.pause();
+    oldVideo.setAttribute('hidden', '');
+
+    newVideo.playbackRate = oldVideo.playbackRate;
+    newVideo.muted = oldVideo.muted;
+    newVideo.controls = false;
+    newVideo.play();
+    newVideo.removeAttribute('hidden');
+    newVideo.controls = true;
+}
+
+function DLV(videoElement, newSrc, seconds, attributes = {}, onEnded = () => { }, defaultSwap = _defaultSwap) {
+    /*
+        `videoElement` must be an `HTMLVideoElement`
+        `newSrc` must be `string` or array of strings
+        `seconds` must be a number
+        `attributes` must be an object (dict)
+    */
     if (!(videoElement instanceof HTMLVideoElement))
         throw new TypeError(`\`videoElement\` must be instance of \`HTMLVideoElement\`, got ${typeof videoElement}`);
     if (typeof newSrc !== 'string' || (Array.isArray(newSrc) && newSrc.some(val => typeof val !== 'string')))
@@ -7,63 +29,47 @@ function DLV(videoElement, newSrc, seconds, attributes = [], onEnded = () => {})
         newSrc = [newSrc];
     if (typeof seconds !== 'number')
         throw new TypeError(`\`seconds\` must be an integer, got ${typeof seconds}`);
-    if (Array.isArray(attributes) && attributes.some(val => val.constructor !== Object))
-        throw new TypeError(`\`attributes\` must be an array of objects, got ${typeof attributes}`);
+    if (attributes.constructor !== Object)
+        throw new TypeError(`\`attributes\` must be a dict, got ${typeof attributes}`);
 
-    return new Promise((res, rej) => {
-        try {
-            const swap = (newVideo, oldVideo, endend = false) => {
-                if(!(newVideo in document.body))
-                    document.body.append(newVideo)
 
-                oldVideo.controls = false;
-                oldVideo.pause();
-                oldVideo.setAttribute('hidden', '');
+    const newVideoElement = document.createElement('video');
+    const swap = (newVideo, oldVideo, ended = false) => {
+        defaultSwap(newVideo, oldVideo);
 
-                newVideo.playbackRate = oldVideo.playbackRate;
-                newVideo.volume = oldVideo.volume;
-                newVideo.controls = false;
-                newVideo.play();
-                newVideo.removeAttribute('hidden');
-                newVideo.controls = true;
 
-                if(endend) {
-                    onEnded();
-                    res();
-                }
-            }
 
-            let newVideoElement = document.createElement('video');
-            newVideoElement.controls = true;
-            
-            attributes.forEach(attr => {
-                const [[key, val]] = Object.entries(attr);
-                if (typeof key !== 'string')
-                    throw new TypeError(`\`newVideo\` attribute keys must be strings, got ${typeof key}`);
+        onEnded(videoElement, newVideoElement, timeUpdatepdateListener, endEventListener);
+    };
+    const timeUpdatepdateListener = () => {
+        const time = Math.floor(videoElement.currentTime);
 
-                newVideoElement.setAttribute(key, val);
-            });
-
-            newSrc = newSrc.reduce((acc, curr) => acc += `<source src="${curr}">`, '');
-            newVideoElement.innerHTML = newSrc;
-
-            videoElement.addEventListener('timeupdate', () => {
-                const time = Math.floor(videoElement.currentTime);
-
-                if(time === seconds) {
-                    swap(newVideoElement, videoElement);
-                    videoElement.currentTime = Math.ceil(videoElement.currentTime);
-                }
-            });
-
-            newVideoElement.addEventListener('ended', swap.bind(this, videoElement, newVideoElement, true));
-        } catch (e) {
-            rej(e);
+        if (time === seconds) {
+            swap(newVideoElement, videoElement);
+            videoElement.currentTime = Math.ceil(videoElement.currentTime);
         }
+    };
+    const endEventListener = swap.bind(this, videoElement, newVideoElement, true);
+
+    // Setting given attributes on `newVideoElement`
+    Object.entries(attributes).forEach(([key, val]) => {
+        if (typeof key !== 'string')
+        throw new TypeError(`\`newVideo\` attribute keys must be strings, got ${typeof key}`);
+        
+        newVideoElement.setAttribute(key, val);
     });
-}
+    
+    // Formatting the given source's
+    newSrc = newSrc.reduce((_, curr) => `<source src="${curr}">`, '');
+    newVideoElement.innerHTML = newSrc;
+    newVideoElement.controls = true;
+
+    // Adding EL's
+    videoElement.addEventListener('timeupdate', timeUpdatepdateListener);
+    newVideoElement.addEventListener('ended', endEventListener);
+};
 
 window.addEventListener('load', () => {
-    DLV(document.querySelector('video'), 'videos/ElephantsDream.mp4', 5).then(console.log.bind(this, 'ended elephantsdream'));
-    DLV(document.querySelector('video'), 'videos/ForBiggerBlazes.mp4', 10).then(console.log.bind(this, 'ended forbiggerblazes'));
+    DLV(document.querySelector('video'), 'videos/ForBiggerEscapes.mp4', 5);
+    DLV(document.querySelector('video'), 'videos/ForBiggerFun.mp4', 10);
 });
